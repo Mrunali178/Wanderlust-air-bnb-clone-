f69a63df4b47cfdfc67d7738e0f8b1502deb219f
Mrunali178 <mrunali.baviskar17@gmail.com>
2025-09-28 17:21:21 +0530

    listing deletion will delete the corresponding reveiws

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+// const { listingSchema } = require("../schema");
 const Schema = mongoose.Schema
+const Review = require("./reviews.js");
 
 const listingSchema = new Schema({
     title:{
@@ -19,7 +21,12 @@ const listingSchema = new Schema({
         type:Schema.Types.ObjectId,
         ref:"Review"
     }],
+    
 });
+listingSchema.post("findOneAndDelete",async (listing)=>{
+    if(listing){
+    await Review.deleteMany({_id:{$in:listing.reviews}});
+}});
 
 const Listing = mongoose.model("Listing",listingSchema);
 module.exports = Listing;
