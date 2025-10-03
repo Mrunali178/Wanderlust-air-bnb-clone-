084153d1efd9e90fa1f6921167d671db60a3df2e
Mrunali178 <mrunali.baviskar17@gmail.com>
2025-10-03 16:26:52 +0530

    login after signup directly

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,7 @@ const passport = require("passport");
 //     }
 // }
 
-router.get("/singup",(req,res)=>{
+router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 })
 
@@ -29,11 +29,17 @@ router.post("/signup",wrapAsync(async(req,res)=>{
         let newUser = new User({email, username});
         let registeredUser = await User.register(newUser,password);
         console.log(registeredUser);
-        req.flash("success","Welcome to Wanderlust");
-        res.redirect("/listings");
+        req.login(registeredUser,(err)=>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success","Welcome to Wanderlust");
+            res.redirect("/listings");
+        })
+      
     }catch(e){
         req.flash("error",e.message);
-        res.redirect("/singup");
+        res.redirect("/signup");
     }
    
 }));
