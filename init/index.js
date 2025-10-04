3e279694536c1f8bc1b8f37c9719df26876f6f5c
Mrunali178 <mrunali.baviskar17@gmail.com>
2025-10-04 07:32:54 +0530

    add owner for each listing

--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ async function main(){
 
 const initDB= async ()=> {
     await Listing.deleteMany({});
+    initData.data=initData.data.map((obj)=>({...obj,owner:"68dde3adde6587c9d9e5a092"}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
