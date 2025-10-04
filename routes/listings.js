15e5952765bb583de7bc77a7669c3c8188638b70
Mrunali178 <mrunali.baviskar17@gmail.com>
2025-10-04 16:55:24 +0530

    middilwares sepration and authorization given

--- routes/listings.js
+++ routes/listings.js
@@ -1,22 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js");
-const { listingSchema } = require("../schema.js");
-const {isLoggedIn} = require("../middileware.js");
-
-//validation middleware
-const validateListing=(req,res,next)=>{
-    let {error} = listingSchema.validate(req.body);
-    
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
+const {isLoggedIn, isOwner, validateListing} = require("../middileware.js");
 
 //index route
 router.get('/',wrapAsync(async (req,res)=>{
@@ -45,7 +31,7 @@ router.post("/",isLoggedIn,validateListing,wrapAsync(async (req,res)=>{
 
 
 //edit route
-router.get("/:id/edit",isLoggedIn, wrapAsync(async(req,res)=>{
+router.get("/:id/edit",isLoggedIn,isOwner, wrapAsync(async(req,res)=>{
     let {id} =req.params;
     let listing = await Listing.findById(id);
     if(!listing){
@@ -56,7 +42,7 @@ router.get("/:id/edit",isLoggedIn, wrapAsync(async(req,res)=>{
 }));
 
 //update route
-router.put("/:id",isLoggedIn,validateListing,wrapAsync(async(req,res)=>{
+router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(async(req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     req.flash("success","Listing updated!");
@@ -78,7 +64,7 @@ router.get("/:id",wrapAsync(async(req,res)=>{
 }));
 
 //delete route
-router.delete("/:id",isLoggedIn,wrapAsync(async(req,res)=>{
+router.delete("/:id",isLoggedIn,isOwner,wrapAsync(async(req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     req.flash("success","Successfully deleted listing!");
