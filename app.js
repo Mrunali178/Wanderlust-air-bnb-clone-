a00fd95f9a30ac5f69e08adcc5a77b10085ceb06
Mrunali178 <mrunali.baviskar17@gmail.com>
2025-10-08 18:30:14 +0530

    delete testing root

--- app.js
+++ app.js
@@ -50,9 +50,9 @@ async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
-app.get('/',(req,res)=>{
-    res.send("root is working");
-});
+// app.get('/',(req,res)=>{
+//     res.send("root is working");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
