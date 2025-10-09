432443cc7122eaaf2af8342eef0ae002e8da1822
Mrunali178 <mrunali.baviskar17@gmail.com>
2025-10-09 09:36:50 +0530

    category addedd

--- schema.js
+++ schema.js
@@ -6,8 +6,10 @@ module.exports.listingSchema = Joi.object({
         location:Joi.string().required(),
         country:Joi.string().required(),
         price:Joi.number().required().min(0),
-        image:Joi.string().allow("",null)
-    }).required()
+        image:Joi.string().allow("",null),
+        category: Joi.string().required()
+    }).required(),
+    
 });
 
 module.exports.reviewSchema = Joi.object({
